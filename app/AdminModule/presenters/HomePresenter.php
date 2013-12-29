0826dddaa9cce91ac1849aa0f7ebdaabd8ba2bcc
Ondřej Profant <ondrej.profant@gmail.com>
2013-12-29 18:24:46 +0100

    Solve some issues avbout Admin:Home

--- app/AdminModule/presenters/HomePresenter.php
+++ app/AdminModule/presenters/HomePresenter.php
@@ -13,19 +13,26 @@ class HomePresenter extends \App\BasePresenter
 
     public function renderDefault()
     {
+		$dsn = explode('=',$this->conn->getConnection()->dsn);
 
-	    $dbname = explode('=',$this->conn->getConnection()->dsn)[2];
-        $tables = $this->conn->query("SHOW FULL TABLES FROM $dbname")->fetchPairs();
-        $result = array();
-        foreach($tables as $name => $type) {
-            $result[$name] = $this->conn->query("SHOW COLUMNS FROM $name")->fetchAll();
-        }
+	    if(isset($dsn[2])) {
+		    $dbname = $dsn[2];
+
+	        $tables = $this->conn->query("SHOW FULL TABLES FROM $dbname")->fetchPairs();
+	        $result = array();
+	        foreach($tables as $name => $type) {
+	            $result[$name] = $this->conn->query("SHOW COLUMNS FROM $name")->fetchAll();
+	        }
+			$this->template->db_info = $result;
+		} else {
+			$this->template->db_info = array();
+		}
 
         $this->template->authors = $this->conn->table('author')->select('id')->count('id');
 	    $this->template->articles = $this->conn->table('article')->select('id')->count('id');
 	    $this->template->tags = $this->conn->table('tag')->select('id')->count('id');
 	    $this->template->resources = $this->conn->table('resource')->select('id')->count('id');
-        $this->template->db_info = $result;
+
         $this->template->php_version = phpversion();
         $this->template->php_extensions = get_loaded_extensions();
     }
